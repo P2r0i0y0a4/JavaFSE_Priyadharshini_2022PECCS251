61dd4d3bf9a842cee7be80194b722d2c99a07c4d
Success <Success@192.168.1.6>
2025-06-20 07:37:55 +0530

    Commit

--- JavaFSE_Priyadharshini_2022PECCS251/src/javafse/DocumentCreator.java
+++ JavaFSE_Priyadharshini_2022PECCS251/src/javafse/DocumentCreator.java
@@ -1,18 +1,13 @@
 package javafse;
-
-
 public class DocumentCreator {
     public static void main(String[] args) {
         System.out.println("Demonstrating Factory Method Pattern:\n");
-
         // Create a Word Document
         DocumentFactory wordFactory = new WordDocumentFactory();
         wordFactory.processDocument(); // Uses the factory method to create and process a WordDocument
-
         // Create a PDF Document
         DocumentFactory pdfFactory = new PdfDocumentFactory();
         pdfFactory.processDocument(); // Uses the factory method to create and process a PdfDocument
-
         // Create an Excel Document
         DocumentFactory excelFactory = new ExcelDocumentFactory();
         excelFactory.processDocument(); // Uses the factory method to create and process an ExcelDocument
